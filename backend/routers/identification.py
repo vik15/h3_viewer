483bcfbb57ef355f3e7c5c318057975156b4c9c0
vik15 <vikram.chennuri@gmail.com>
2026-07-07 15:08:18 +0530

    new api endpoints in identifcation

--- backend/routers/identification.py
+++ backend/routers/identification.py
@@ -4,6 +4,31 @@ import h3
 router = APIRouter(prefix="/identify")
 
 
+@router.get("/is_valid_cell")
+def is_valid_cell(cell):
+    return h3.is_valid_cell(cell)
+
+
 @router.get("/is_pentagon")
 def is_pentagon(cell):
     return h3.is_pentagon(cell)
+
+
+@router.get("/is_res_class_III")
+def is_res_class_III(cell):
+    return h3.is_res_class_III(cell)
+
+
+@router.get("/is_valid_directed_edge")
+def is_valid_directed_edge(edge):
+    return h3.is_valid_directed_edge(edge)
+
+
+@router.get("/is_valid_vertex")
+def is_valid_vertex(vertex):
+    return h3.is_valid_vertex(vertex)
+
+
+@router.get("/is_valid_index")
+def is_valid_index(cell):
+    return is_valid_index(cell)
